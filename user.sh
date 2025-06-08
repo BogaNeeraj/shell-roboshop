267da027e797ffe43d9003e604f91703f0339182
Neeraj <neerajchintwo@gmail.com>
2025-06-08 23:31:02 +0530

    conditions

--- user.sh
+++ user.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Starting cart"
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo " script executed successfully, $Y time taken : $TOTAL_TIME seconds $N " | tee -a $LOG_FILE
+ echo -e "script executed successfully, $Y time taken : $TOTAL_TIME seconds $N " | tee -a $LOG_FILE
 # 
 #!/bin/bash
 # START_TIME=$(date +%s)
