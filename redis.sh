84534e12509d11cd7a1f64f6fe57673d3529e137
Neeraj <neerajchintwo@gmail.com>
2025-06-08 18:00:51 +0530

    conditions

---
+++ redis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/roboshop.logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+ echo "script started at : $TIMESTAMP " | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then 
+ echo -e "$R error:: please run the script with the root user $N"
+else
+ echo -e "$G script started with root user $N"
+fi
+
+VALIDATE(){
+if [ $1 -eq 0 ]
+then 
+ echo -e "$2...$G success $N" | tee -a $LOG_FILE
+
+else 
+ echo -e "$2... $R Failure $N" | tee -a $LOG_FILE
+fi 
+}
+
+dnf module disable redis -y
+VALIDATE $? "Disabling the redis"
+dnf module enable redis:7 -y
+VALIDATE "Enabling the redis"
+
+dnf install redis -y 
+VALIDATE $? "Install redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no ' /etc/redis/redis.conf
+
+systemctl enable redis 
+systemctl start redis 
+VALIDATE $? "Staring the redis"
+
+
+
