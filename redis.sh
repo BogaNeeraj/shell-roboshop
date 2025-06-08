a1c52d6cfebc6a42994de43b34010aa1a7dc0ba6
Neeraj <neerajchintwo@gmail.com>
2025-06-08 18:02:05 +0530

    conditions

--- redis.sh
+++ redis.sh
@@ -33,18 +33,18 @@ else
 fi 
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>> $LOG_FILE
 VALIDATE $? "Disabling the redis"
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>> $LOG_FILE
 VALIDATE "Enabling the redis"
 
-dnf install redis -y 
+dnf install redis -y &>> $LOG_FILE
 VALIDATE $? "Install redis"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no ' /etc/redis/redis.conf
 
-systemctl enable redis 
-systemctl start redis 
+systemctl enable redis &>> $LOG_FILE
+systemctl start redis &>> $LOG_FILE
 VALIDATE $? "Staring the redis"
 
 
